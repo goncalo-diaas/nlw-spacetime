4d9acb16cdddecf14d099f1d67d83bc78f43dce6
Gonçalo Dias <goncalodia17@gmail.com>
2023-05-19 15:47:23 +0100

    aula 04

--- mobile/app/memories.tsx
+++ mobile/app/memories.tsx
@@ -1,9 +1,11 @@
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+import { Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { View, Text } from "react-native";
 
 export default function Memories() {
-    return (
-        <View className="flex-1 items-center justify-center">
-            <Text>Memories</Text>
-        </View>
-    )
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Memories</Text>
+    </View>
+  );
 }
